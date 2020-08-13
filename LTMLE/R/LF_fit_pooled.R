d0fda36503edf65ed707e3529c9a531e2bc365a2
Lars van der Laan <larsvanderlaan@berkeley.edu>
2020-08-13 14:42:15 -0700

    sl3 subsemble package made

---
+++ LTMLE/R/LF_fit_pooled.R
@@ -0,0 +1,195 @@
+#' Likelihood Factor Estimated from Data using sl3.
+#'
+#' Uses an \code{sl3} learner to estimate a likelihood factor from data.
+#' Similar to LF_fit object but allows vector-valued names (corresponding to multiple nodes sharing the same regression_task variables but at different times)
+#' This learner will pool the regression tasks across time. This depends on ltmle3_Task which has the pooled task functionality.
+#' Inherits from \code{\link{LF_base}}; see that page for documentation on likelihood factors in general.
+#'
+#' @importFrom R6 R6Class
+#' @importFrom uuid UUIDgenerate
+#' @importFrom methods is
+#' @family Likelihood objects
+#' @keywords data
+#'
+#' @return \code{LF_base} object
+#'
+#' @format \code{\link{R6Class}} object.
+#'
+#' @section Constructor:
+#'   \code{define_lf(LF_fit, name, learner, ..., type = "density")}
+#'
+#'   \describe{
+#'     \item{\code{name}}{character, the name of the factor. Should match a node name in the nodes specified by \code{\link{tmle3_Task}$npsem}
+#'     }
+#'     \item{\code{learner}}{An sl3 learner to be used to estimate the factor
+#'     }
+#'     \item{\code{...}}{Not currently used.
+#'     }
+#'     \item{\code{type}}{character, either "density", for conditional density or, "mean" for conditional mean
+#'     }
+#'     }
+#'
+#' @section Fields:
+#' \describe{
+#'     \item{\code{learner}}{The learner or learner fit object}
+#'     }
+#' @export
+LF_fit_pooled <- R6Class(
+  classname = "LF_fit_pooled",
+  portable = TRUE,
+  class = TRUE,
+  inherit = LF_base,
+  public = list(
+    initialize = function(name, learner, is_time_variant = FALSE, ..., type = "density") {
+      super$initialize(name, ..., type = type)
+      private$.learner <- learner
+      # TODO: add parameter is_time_variant
+      private$.is_time_variant <- is_time_variant
+    },
+    delayed_train = function(tmle_task) {
+      # just return prefit learner if that's what we have
+      # otherwise, make a delayed fit and return that
+      if (self$learner$is_trained) {
+        return(self$learner)
+      }
+
+      outcome_node <- self$name
+
+      # fit scaled task for bounded continuous
+      learner_task <- tmle_task$get_regression_task(outcome_node, scale = TRUE, drop_censored=TRUE,
+                                                    is_time_variant = self$is_time_variant)
+      learner_fit <- delayed_learner_train(self$learner, learner_task)
+      return(learner_fit)
+    },
+    train = function(tmle_task, learner_fit) {
+      super$train(tmle_task)
+      private$.learner <- learner_fit
+    },
+    shape_predictions = function(tmle_task, preds){
+      # Reshapes predictions if this is a pooled_task
+      # returns the predictions as n x len(self$names) data.table.
+      if(length(self$names)==1){
+        return(preds)
+      }
+      ids <- tmle_task$get_node("id")
+      preds_list  <- lapply(unique(ids), function(id){
+        # Returns the vector of predictions for a single person
+        preds[which(ids == id)]
+      })
+      # Stack preds so each row corresponds to the prediction for a single person
+      preds_stacked <- data.table(do.call(rbind, preds_list))
+      setnames(preds_stacked, self$names)
+      return(preds_stacked)
+    },
+    get_mean = function(tmle_task, fold_number) {
+      # TODO: prediction is made on all data, so is_time_variant is set to TRUE
+      learner_task <- tmle_task$get_regression_task(self$name, is_time_variant = TRUE)
+      learner <- self$learner
+      preds <- learner$predict_fold(learner_task, fold_number)
+
+      # unscale preds (to handle bounded continuous)
+      preds_unscaled <- tmle_task$unscale(preds, self$name)
+      preds <- shape_predictions(tmle_task, preds)
+      return(preds)
+    },
+    get_density = function(tmle_task, fold_number) {
+      # TODO: prediction is made on all data, so is_time_variant is set to TRUE
+      learner_task <- tmle_task$get_regression_task(self$name, is_time_variant = TRUE)
+      learner <- self$learner
+      preds <- learner$predict_fold(learner_task, fold_number)
+
+      outcome_type <- self$learner$training_task$outcome_type
+      observed <- outcome_type$format(learner_task$Y)
+      if (outcome_type$type == "binomial") {
+        likelihood <- ifelse(observed == 1, preds, 1 - preds)
+      } else if (outcome_type$type == "categorical") {
+        unpacked <- sl3::unpack_predictions(as.vector(preds))
+        index_mat <- cbind(seq_along(observed), observed)
+        likelihood <- unpacked[index_mat]
+      } else if (outcome_type$type == "continuous") {
+        likelihood <- unlist(preds)
+      } else {
+        stop(sprintf("unsupported outcome_type: %s", outcome_type$type))
+      }
+      likelihood <- shape_predictions(tmle_task, likelihood)
+      return(likelihood)
+    },
+    sample = function(tmle_task, n_samples = NULL, fold_number = "full") {
+      warning("This function has not been implemented for pooled tasks.")
+      # TODO: fold
+      # TODO: pooled tasks
+      if (is.null(tmle_task)) {
+        tmle_task <- self$training_task
+      }
+      if (is.null(n_samples)) {
+        return(tmle_task)
+      }
+
+      learner_task <- tmle_task$get_regression_task(self$name)
+      learner <- self$learner
+
+      outcome_type <- learner$training_task$outcome_type
+
+      if (outcome_type$type == "binomial") {
+        # TODO: option to return task
+        # TODO: think about how folds should be structured on resample
+        # need to keep ids the same
+        # probably also predict using training set fits
+        preds <- learner$predict_fold(learner_task, "full")
+        values <- sapply(preds, function(p) rbinom(n_samples, 1, p))
+      } else if (outcome_type$type == "categorical") {
+        preds <- learner$predict_fold(learner_task, "full")
+        unpacked <- sl3::unpack_predictions(as.vector(preds))
+        values <- apply(
+          unpacked, 1,
+          function(probs) {
+            apply(
+              rmultinom(n_samples, 1, probs) == 1, 2,
+              function(onehots) outcome_type$levels[which(onehots)]
+            )
+          }
+        )
+      } else if (outcome_type$type == "continuous") {
+        if ("sampling" %in% learner$properties) {
+          values <- learner$sample(learner_task, n_samples, "full")
+        } else {
+          values <- matrix(nrow = n_samples, ncol = tmle_task$nrow)
+          for (i in 1:tmle_task$nrow) {
+            subject <- tmle_task[i]
+            f_X <- function(a) {
+              cf_data <- data.table(a)
+              setnames(cf_data, names(cf_data), self$name)
+              subject_a <- subject$generate_counterfactual_task(UUIDgenerate(), cf_data)
+
+              pred <- learner$predict_fold(subject_a$get_regression_task(self$name), "full")
+              likelihood <- unlist(pred)
+
+              return(likelihood)
+            }
+            samples <- AR.Sim(n_samples, f_X,
+                              xlim = c(min(learner$training_task$Y), max(learner$training_task$Y))
+            )
+            values[, i] <- samples
+          }
+        }
+      } else {
+        stop(sprintf("unsupported outcome_type: %s", outcome_type$type))
+      }
+      values <- t(values)
+      return(values)
+    }
+  ),
+  active = list(
+    learner = function() {
+      return(private$.learner)
+    },
+    is_time_variant = function() {
+      return(private$.is_time_variant)
+    }
+  ),
+  private = list(
+    .name = NULL,
+    .learner = NULL,
+    .is_time_variant = NULL
+  )
+)
---
+++ LTMLE/R/Likelihood_pooled.R
@@ -0,0 +1,89 @@
+
+
+#' Likelihood object that supports a list of pooled LF objects (e.g. LF_fit_pooled)
+#' All output behavior is identical to the standard Likelihood object.
+#' @export
+Likelihood_pooled <- R6Class(
+  classname = "Likelihood_pooled",
+  portable = TRUE,
+  class = TRUE,
+  inherit = Likelihood,
+  active = list(
+    factor_list_unpooled = function(){
+      self$params$factor_list_unpooled
+    }
+  ),
+  public = list(
+    initialize = function(factor_list, cache = NULL, ...) {
+      params <- args_to_list()
+      if (inherits(factor_list, "LF_base")) {
+        factor_list <- list(factor_list)
+      }
+
+      factor_names <- lapply(factor_list, `[[`, "name")
+      factor_list_unpooled <- list()
+      for(i in seq_along(factor_names)){
+        names <- c(factor_names[[i]])
+        factor <- factor_list[[i]]
+        for(name in names){
+          factor_list_unpooled$name <- factor
+        }
+      }
+      names(factor_list) <-  sapply(factor_names, function(name) paste(name, collapse = "%"))
+      #factor_list is used for training
+      #factor_list_unpooled is used for prediction
+      #Assumes that training is done via mutation
+      params$factor_list <- factor_list
+      params$factor_list_unpooled <- factor_list_unpooled
+      if (is.null(cache)) {
+        cache <- Likelihood_cache$new()
+      }
+      private$.cache <- cache
+
+      super$initialize(params)
+    },
+    validate_task = function(tmle_task) {
+      assert_that(is(tmle_task, "tmle3_Task"))
+
+      factor_list <- self$factor_list
+      factor_names <- unlist(lapply(factor_list, `[[`, "name"))
+      task_nodes <- names(tmle_task$npsem)
+      if (!all(factor_names %in% task_nodes)) {
+        stop("factor_list and task$npsem must have matching names")
+      }
+    },
+    get_likelihood = function(tmle_task, node, fold_number = "full") {
+      likelihood_factor <- self$factor_list_unpooled[[node]]
+      # first check for cached values for this task
+
+      likelihood_values <- self$cache$get_values(likelihood_factor, tmle_task, fold_number)
+      # note the above contains all values from the pooled task, not just this node.
+
+      if (is.null(likelihood_values)) {
+        # if not, generate new ones
+        likelihood_values <- likelihood_factor$get_likelihood(tmle_task, fold_number)
+        self$cache$set_values(likelihood_factor, tmle_task, 0, fold_number, likelihood_values)
+      }
+      #Subset to only likelihood values of this node
+      likelihood_values <- likelihood_values[,node, with = F]
+
+      return(likelihood_values)
+    },
+    get_likelihoods = function(tmle_task, nodes = NULL, fold_number = "full") {
+      if (is.null(nodes)) {
+        nodes <- self$nodes
+      }
+
+      if (length(nodes) > 1) {
+        all_likelihoods <- lapply(nodes, function(node) {
+          self$get_likelihood(tmle_task, node, fold_number)
+        })
+        likelihood_dt <- as.data.table(all_likelihoods)
+        setnames(likelihood_dt, nodes)
+        return(likelihood_dt)
+      } else {
+        return(self$get_likelihood(tmle_task, nodes[[1]], fold_number))
+      }
+    }
+  )
+)
